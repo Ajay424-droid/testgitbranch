d422ed4d4efa00654da1b9d03699299da14edf17
Ajay <ap1892503@gmail.com>
2025-12-26 16:22:04 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello Git Practice"
+
+@app.route("/about")
+def about():
+    return "This is a small Flask app"
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
+
